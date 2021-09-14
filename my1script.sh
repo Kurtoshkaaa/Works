b7e9581d01b5d183874dc96ae1d466a005a4d4ad
Aliaksey Kurto <kurtoshka1993@gmail.com>
2021-09-14 23:25:12 +0300

    hmwrk

---
+++ my1script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd COURSES
+mkdir 1_folder
+mkdir 2_folder
+mkdir 3_folder
+cd 1_folder
+touch 1.txt
+touch 2.txt
+touch 3.txt
+touch 1.json
+touch 2.json
+mkdir a
+mkdir b
+mkdir c
+ls
+mv 1.txt 2.txt a
