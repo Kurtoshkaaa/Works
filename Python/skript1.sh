ceb45205e35d9dfb8ead2488896ba3fc80c8dd11
Aliaksey Kurto <89684354+Kurtoshkaaa@users.noreply.github.com>
2021-11-05 12:08:16 +0300

    updt

--- Python/skript1.sh
+++ Python/skript1.sh
@@ -1,5 +1,16 @@
-<<<<<<< HEAD
-python3 "PYTHON_HOMEWORK(3.4).py"
-=======
-python3 "PYTHON_HOMEWORK(3.4).py"
->>>>>>> 710d4a7e3b8a16bb7022f004c64cd0503279ada9
+#!/bin/bash
+cd COURSES
+mkdir 1_folder
+mkdir 2_folder
+mkdir 3_folder
+cd 1_folder
+touch 1.txt
+touch 2.txt
+touch 3.txt
+touch 1.json
+touch 2.json
+mkdir a
+mkdir b
+mkdir c
+ls
+mv 1.txt 2.txt a
